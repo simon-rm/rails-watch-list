0ec47d3557fc4b069c2cb4af9560a55da53e2995
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-07 02:25:36 -0300

    index working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+50.times do
+movie = Movie.create(
+  title:Faker::Movie.title,
+  overview:Faker::Lorem.sentence(word_count: 30),
+  poster_url: Faker::LoremFlickr.image(size: "300x150"),
+  rating: rand(0..5)
+  )
+end
