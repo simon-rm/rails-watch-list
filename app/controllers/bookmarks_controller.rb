3aea74fdeeae1c98cc6cc8b523ddaab787c8ef76
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-07 00:44:20 -0300

    created bookmark controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,15 @@
 class BookmarksController < ApplicationController
+  def create
+    @bookmark = Bookmark.new bookmark_params :name
+    @bookmark.movie = Movie.find bookmark:movie_id
+    @bookmark.movie_list = params :movie_list_id
+    @bookmark.movie = bookm
+    if @bookmark.save
+      redirect_to "movie_lists/show"
+    end
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:name, :movie_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   resources :movie_lists, only: %i[index show update create] do
     resources :bookmark, only: %i[create]
-    member do
-      get "bookmark/new"
-    end
   end
 end
